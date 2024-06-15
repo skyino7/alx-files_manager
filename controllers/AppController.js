3202152faa5fe5e423e29907d3eeed630ec20911
Seth Ketsi <christambassador12@gmail.com>
2024-06-15 16:01:18 +0000

    updated

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,6 +2,14 @@ import redisClient from "../utils/redis";
 import dbClient from "../utils/db";
 
 class AppController {
+
+  /**
+   * Retrieves the status of the Redis client and the database client and sends a JSON response with the counts.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @return {Promise<void>} A Promise that resolves when the response is sent, or rejects with an error.
+   */
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
@@ -9,6 +17,13 @@ class AppController {
     });
   }
 
+  /**
+   * Retrieves the number of users and files in the database and sends a JSON response with the counts.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @return {Promise<void>} A Promise that resolves when the response is sent, or rejects with an error.
+   */
   static async getStats(req, res) {
     try {
       const users = await dbClient.nbUsers();
