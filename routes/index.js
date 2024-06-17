af47e819ce0bea55c56726f1c468fc9a2eff45d0
Seth Ketsi <christambassador12@gmail.com>
2024-06-17 10:39:53 +0000

    Implementing the Files Contoller and added the postupload to the routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import AppController from "../controllers/AppController.js";
 import UsersController from "../controllers/UsersController.js";
 import AuthController from "../controllers/AuthController.js";
+import FilesController from "../controllers/FilesController.js"
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.post("/users", UsersController.postNew);
 router.get("/connect", AuthController.getConnect);
 router.get("/disconnect", AuthController.getDisconnect);
 router.get("/users/me", UsersController.getMe);
+router.get("/files", FilesController.postUpload);
 
 export default router;
