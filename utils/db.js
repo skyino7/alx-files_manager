e9a5c2e358de38f0435a56ccf21b89ee7d41fae1
Seth Ketsi <christambassador12@gmail.com>
2024-06-19 10:10:58 +0000

    This works instead

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,73 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 class DBClient {
+  /**
+     * Constructor for the DBClient class.
+     *
+     * Connects to a MongoDB database using the provided environment variables or default values.
+     * If the connection is successful, the `db` property is set to the connected database.
+     * If the connection fails, the `db` property is set to `null` and an error message is logged.
+     *
+     * @constructor
+     */
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
+    const port = process.env.DB_PORT || '27017';
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${dbName}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
-    const url = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.client.connect().then(() => {
-      this.db = this.client.db(database);
-      console.log('Connected to MongoDB');
-    }).catch((err) => {
-      console.error('Failed to connect to MongoDB', err);
-    });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(dbName);
+      })
+      .catch((err) => {
+        console.error(`Error connecting to MongoDB: ${err.message}`);
+        this.db = null;
+      });
   }
 
+  /**
+     * Checks if the MongoDB client is connected.
+     *
+     * @return {boolean} Returns true if the client is connected, false otherwise.
+     */
   isAlive() {
-    return this.client.topology && this.client.topology.isConnected();
+    return this.client.isConnected();
   }
 
+  /**
+     * Asynchronously retrieves the number of users in the 'users' collection.
+     *
+     * @return {Promise<number|null>} The number of users, or null if the
+     * database connection is not established.
+     */
   async nbUsers() {
-    if (this.isAlive()) {
-      return this.db.collection('users').countDocuments();
+    if (!this.db) {
+      return null;
     }
-    return 0;
+    const result = await this.db
+      .collection('users')
+      .countDocuments();
+    return result;
   }
 
+  /**
+     * Asynchronously retrieves the number of files in the 'files' collection.
+     *
+     * @return {Promise<number|null>} The number of files, or null
+     * if the database connection is not established.
+     */
   async nbFiles() {
-    if (this.isAlive()) {
-      return this.db.collection('files').countDocuments();
+    if (!this.db) {
+      return null;
     }
-    return 0;
+    const result = await this.db
+      .collection('files')
+      .countDocuments();
+    return result;
   }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
